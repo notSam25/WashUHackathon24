a915a561669313f0abbbc3c910dc2b48620b73ec
Noah <noahjw23@icloud.com>
2024-10-19 21:43:57 -0500

    backend.py made restAPI

---
+++ backend.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
